b8ec84d046706643a59a1874a6bd0a350988ee60
Sahil Behera <sahilbehera111333@gmail.com>
2026-03-21 14:23:57 +0530

    added validation annotations to account request dto

--- src/main/java/com/sahil/ledger_module/dto/AccountRequest.java
+++ src/main/java/com/sahil/ledger_module/dto/AccountRequest.java
@@ -2,10 +2,17 @@ package com.sahil.ledger_module.dto;
 
 import java.math.BigDecimal;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class AccountRequest {
+    @NotBlank
     private String accountName;
+
+    @NotNull
+    @Positive
     private BigDecimal balance;
 }
